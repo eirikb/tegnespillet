571e69f1e5d05ac98664de56e099d26c7afc4f61
Eirik Brandtzæg <eirikb@eirikb.no>
2016-10-20 18:02:20 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+          <a id="home" href="/">Home</a>
+          <component :is="state.name" :store="store" :state="state"></component>
+  </div>
+</template>
+
+<script>
+    import states from './states';
+    export default {
+        components: Object.assign({}, states),
+        props: ['store'],
+        data() {
+            return {
+                state: this.store.getState()
+            };
+        },
+        created() {
+            this.store.subscribe(() => this.state = this.store.getState());
+        }
+    };
+</script>
+
+<style lang="sass">
+    html,
+    body {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+    }
+</style>
---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log('SO say we all');
+import 'babel-polyfill';
+import Vue from 'vue';
+import App from './App.vue';
+
+// import 'es6-promise';
+// import 'whatwg-fetch';
+
+import reducer from './reducers';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+new Vue({
+  el: '#app',
+  render: h => h(App, {
+    props: {
+      store: store
+    }
+  })
+});
