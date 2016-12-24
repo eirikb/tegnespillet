2bf0cb2c65ef3724ba2c388b9fdea3b62ab873ee
Eirik Brandtzæg <eirikb@eirikb.no>
2016-12-24 16:41:22 +0000

    More words

--- src/states/Words.vue
+++ src/states/Words.vue
@@ -5,12 +5,94 @@
 </template>
 
 <script>
-  import { db } from '../fb';
+  import {
+    db
+  }
+  from '../fb';
 
   export default {
     created() {
       db.ref('words/english').set(['Car', 'Seldgehammer', 'Thief', 'Snake', 'Brain', 'Book', 'Pipe', 'Sun', 'Moon', 'Africa', 'Donald Trump', 'Road', 'USA', 'Cheese slicer']);
-      db.ref('words/norsk').set(['Båt', 'Hus', 'Blomst', 'Kake', 'Sverige', 'Norge', 'Kamel', 'Dromedar', 'Bie', 'Katt', 'Hund', 'Pølse', 'Donald Trump', 'Johaug', 'Avento', 'Ubåt', 'Hval', 'Rundkjøring', 'Ålesund', 'Joggesko', 'Saks', 'Melk']);
+      db.ref('words/norsk').set(['Båt', 'Hus', 'Blomst', 'Kake', 'Sverige', 'Norge', 'Kamel', 'Dromedar', 'Bie', 'Katt', 'Hund', 'Pølse', 'Donald Trump', 'Johaug', 'Avento', 'Ubåt', 'Hval', 'Rundkjøring', 'Ålesund', 'Skolisse', 'Saks', 'Melk', 'Blomsterpotte', 'Sommerfugl', 'Hoppeslott',
+        'Appelsin',
+        'Arm',
+        'Ark',
+        'Apekatt',
+        'Askebeger',
+        'Akebrett',
+        'Arfika',
+        'Amerika',
+        'Australia',
+        'Bart',
+        'Bæsj',
+        'Boksehanske',
+        'Dass',
+        'Dilbert',
+        'Domino',
+        'Fugl',
+        'Fakkel',
+        'Flaggstang',
+        'Fiskestang',
+        'Fiskepinner',
+        'Foss',
+        'Fiskekort',
+        'Garasje',
+        'Gris',
+        'Gangamstyle',
+        'Gullerot',
+        'Gress',
+        'Gjerde',
+        'Hest',
+        'Hellig',
+        'Hulder',
+        'Hatt',
+        'Jakke',
+        'Kalkulator',
+        'Kamera',
+        'Klokke',
+        'Kakkerlakk',
+        'Lampe',
+        'Lege',
+        'Lommerusk',
+        'Lus',
+        'Limpinne',
+        'Lysestake',
+        'Mammut',
+        'Mars',
+        'Meksikaner',
+        'Murvegg',
+        'Mus',
+        'Murstein',
+        'Nisse',
+        'Nilen',
+        'Pakke',
+        'Petrine',
+        'Potet',
+        'Palle',
+        'Pepperkakemann',
+        'Pizza',
+        'Pinnsvin',
+        'Rev',
+        'Rakett',
+        'Romvesen',
+        'Rompeakebrett',
+        'Sag',
+        'Sangbok',
+        'Traktor',
+        'Ulv',
+        'Ugle',
+        'Vekt',
+        'Verden',
+        'Vaktbod',
+        'Vindussprosse',
+        'Ylvisbrørdrene',
+        'Kanin',
+        'Katapult',
+        'Kraftverk',
+        'Prest',
+        'Telefon',
+        'Troll'
+      ]);
       db.ref('words/voksne').set(['Tisselur', 'Fisk', 'Banan', 'Meloner', 'Apekatt', 'Tungekyss', 'Fødsel', 'Sikle', 'Hender', 'Rumpe', 'Rygg', 'Navle', 'Statskupp', 'Tuba']);
     }
   };
