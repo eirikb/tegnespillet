372990a1a86adfd0595eb976c1eae7073b0eb2b5
Eirik Brandtzæg <eirikb@eirikb.no>
2016-12-26 12:38:04 +0000

    Undo-button

--- src/states/Draw.vue
+++ src/states/Draw.vue
@@ -5,6 +5,7 @@
     <canvas ref="canvas"></canvas>
     <div class="bottom" ref="button">
       <button @click="clear">Slett</button>
+      <button @click="undo">Angre</button>
     </div>
   </div>
 </template>
@@ -25,15 +26,24 @@
       let button = this.$refs.button;
 
       const resizeCanvas = () => {
-        let ratio = 1;
-        canvas.width = window.innerWidth * ratio;
-        canvas.height = button.offsetTop - canvas.offsetTop * ratio;
+        // const ratio = Math.max(window.devicePixelRatio || 1, 1);
+        // canvas.getContext("2d").scale(ratio, ratio);
+        canvas.width = window.innerWidth * 1;
+        canvas.height = button.offsetTop - canvas.offsetTop * 1;
+        // canvas.width = canvas.offsetWidth * ratio;
+        // canvas.height = canvas.offsetHeight * ratio;
+        // canvas.getContext("2d").scale(ratio, ratio);
         this.drawing.clear();
       };
       window.onresize = resizeCanvas;
       this.drawing = new signature_pad(canvas, {
         backgroundColor: 'rgb(255, 255, 255)'
       });
+
+      this.memory = [];
+      this.drawing.onEnd = (a, b, c) => {
+        this.memory.push(this.drawing.toDataURL());
+      };
       resizeCanvas();
     },
 
@@ -44,7 +54,25 @@
 
     methods: {
       clear() {
-        this.drawing.clear();
+        if (confirm('Er du sikker?')) {
+          this.drawing.clear();
+        }
+      },
+
+      undo() {
+        const canvas = this.$refs.canvas;
+        const ctx = canvas.getContext('2d');
+        const image = new Image();
+        this.memory.pop();
+        if (this.memory.length === 0) {
+          this.drawing.clear();
+          return;
+        }
+        image.src = this.memory[this.memory.length - 1];
+        image.onload = () => {
+          this.drawing.clear();
+          ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+        };
       }
     }
   };
